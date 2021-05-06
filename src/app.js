0da4973daf7ba05a923a512f9f19e647843678fb
luytngo0904 <83644376+luytngo0904@users.noreply.github.com>
2021-05-06 20:43:43 +0700

    Add function required file

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var fs = require("fs");
+var pathFs = require("path");
 
 var indexRouter = require("./routes/index");
 var testRouter = require("./routes/test");
@@ -21,6 +23,24 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/test", testRouter);
+
+var models_path = pathFs.join(__dirname, 'models');
+
+var requiredFile = function (path, regex) {
+  const files = fs.readdirSync(path);
+  files.forEach((file) {
+    const newPath = path + '/' + file;
+    const stat = fs.statSync(newPath);
+    if (stat.isFile()){
+      if (regex.test(file)) {
+        require(newPath);
+      };
+    };
+  });
+};
+
+const reg = /(.*)\.(model)\.(js$)/;
+requiredFile(models_path, reg);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
