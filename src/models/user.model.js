f358ec10c735f60065fcb13934167c3b0b0a01fe
tien.nguyen5 <tien.nguyen@dimi.vn>
2021-05-07 09:39:02 +0700

    CRUD Board

---
+++ src/models/user.model.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name : {
+        type : String,
+    },
+    password : {
+        type : String,
+        trim : true,
+    },
+    email : {
+        type : String,
+        trim : true
+    },
+    avatar : {
+        type : String,
+        trim : true,
+    },
+    role_id : {
+        type : String,
+        ref : ""
+    }
+});
+
+userSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+    delete ret._id;
+  },
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
---
+++ src/routes/board.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const Board = require("../models/board.model");
+const router = express.Router();
+
+//get all board
+router.get('/', async function(req, res, next) {
+    try {
+        const boards = await Board.find()
+        .populate({ path : "created_by" });
+        res.status(200).json({
+            data: boards,
+            message : "get all board successfully !",
+        }) 
+    } catch (error) {
+        res.status(500).json({
+            error,
+            message : "get all board fail !",
+        })
+    }
+});
+
+//get board by id
+router.get('/:id', async function(req, res, next){
+    try {
+        const boardID = req.params.id;
+        const board = await Board.findById({boardID})
+        .populate({ path : "created_by" })
+        res.status(200).json({
+            data: board,
+            message : "get board id successfully !",
+        })
+    } catch (error) {
+        res.status(500).json({
+            error,
+            message : "get board id fail !",
+        })
+    }
+});
+
+//create board 
+router.post('/create', async function(req, res, next){
+    try {
+        const board = await Board.create(req.body);
+        res.status(200).json({
+            data: board,
+            message : "created board successfully !",
+        })
+    } catch (error) {
+        res.status(500).json({
+            error,
+            message : "created board fail !",
+        })
+    }
+});
+
+//update board 
+router.put("/update/:id", async function(req, res, next){
+    try {
+        const boardID = req.params.id;
+        const boardUpdate = req.body;
+        const board = await Board.findOneAndUpdate(
+            { _id: boardID },{$set : boardUpdate}
+        );
+        res.status(200).json({
+            data : board,
+            message : "update board successfully !"
+        });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+});
+
+//delete board
+router.delete("/delete/:id", async function(req, res, next){
+    try {
+        let boardID = req.params.id;
+        await Board.findByIdAndDelete({_id : boardID});
+        res.status(200).json({
+            message : "deleted board successfully !"
+        })
+    } catch (error) {
+        res.status(500).json({
+            message : "deleted board fail !",
+        })
+    }
+})
+
+module.exports = router;
