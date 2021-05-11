62f13d5e61e3e3d75dd81f2f9a7f2ce5fe223cec
huy <okey222k@gmail.com>
2021-05-11 11:06:42 +0700

    CRUD list and task

---
+++ src/model/list.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const listSchema = new Schema({
+    list_index:{
+        type:String,
+        required:true,
+        default:0
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    board_id:{
+        type:String,
+        ref:"board"
+    }
+},
+    {timestamps:true}
+)
+listSchema.set("toJSON", {
+    virtuals: true,
+    versionKey: false,
+    transform: function (doc, ret) {
+      delete ret._id;
+    },
+  });
+  
+  const List = mongoose.model("list", listSchema);
+  
+  module.exports = List;
+  
