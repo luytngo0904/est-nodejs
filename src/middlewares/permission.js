96ea41256d2bd1804951a4e485492520fe8c627e
tien.nguyen5 <tien.nguyen@dimi.vn>
2021-05-25 11:29:49 +0700

    fix verify token

--- src/middlewares/permission.js
+++ src/middlewares/permission.js
@@ -1,16 +1,21 @@
-const jwt_decode = require("jwt-decode");
+// const jwt_decode = require("jwt-decode");
 const Board = require("../models/board.model");
+const jwt = require("jsonwebtoken");
 module.exports.checkPermission = (arrRole) => async (req, res, next) => {
     let flag = false;
-    const { _id } = jwt_decode(req.headers.authorization.split(" ")[1]);
-    const result = await Board.findOne({"role_in_board.userID" : _id});
-    result.role_in_board.map( val => {
-        if(arrRole.includes(val.role)){
-            flag = true;
-            next();
+    const token = req.headers.authorization.split(" ")[1];
+    const jwt_decode =  jwt.verify(token, process.env.JWT_SECRET);
+    const userID = jwt_decode._id;
+    const board = await Board.findById(req.params.id);
+    board.role_in_board.map( el => {
+        if(el.userID.toString() === userID.toString()){
+            if(arrRole.some(value => value === el.role)){
+                flag = true;
+                next();            
+            }
         }
     })
-    if(!_id || flag === false){
+    if(!userID || flag === false){
         res.status(400).json({
             error : "",
             message : "not allowed"
--- src/routes/boards.js
+++ src/routes/boards.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const _ = require("lodash");
 const Board = require("../models/board.model");
 const User = require("../models/user.model");
 const router = express.Router();
