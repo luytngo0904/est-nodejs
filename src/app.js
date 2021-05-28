d77e2de1e1c5f3664c1ca61a89e90172cd917f91
okey222k <75199998+okey222k@users.noreply.github.com>
2021-05-28 18:47:55 +0700

    Revert "crud for list and task"

--- src/app.js
+++ src/app.js
@@ -7,13 +7,9 @@ const logger = require("morgan");
 const fs = require("fs");
 const pathFs = require("path");
 
-var testRouter = require("./routes/test");
 const indexRouter = require("./routes/index");
 const boardRouter = require("./routes/boards");
 const verifyToken = require("./middlewares/auth");
-var listRouter = require("./routes/lists")
-var taskRouter = require("./routes/tasks")
-
 
 const app = express();
 
@@ -27,13 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(pathFs.join(__dirname, "public")));
 
-
-var models_path = pathFs.join(__dirname, "models");
 app.use("/", verifyToken, indexRouter);
 app.use("/api/boards", verifyToken, boardRouter);
-app.use("/api/test", verifyToken,testRouter);
-app.use("/api/lists",verifyToken, routerList);
-app.use("/api/tasks",verifyToken, routerTask);
 
 // eslint-disable-next-line camelcase
 const models_path = pathFs.join(__dirname, "models");
